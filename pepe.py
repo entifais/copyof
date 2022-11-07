e88adcc146c1414e8446437724935ae53b955174
jero98772 <jero98772@protonmail.com>
2022-11-07 15:22:00 -0500

    update

--- pepe.py
+++ pepe.py
@@ -3,28 +3,35 @@
 #commap - by jero98772
 from flask import Flask, render_template, request, flash, redirect ,session
 import os
-import pandas as pd
 from tools.tools import *
 app = Flask(__name__)
-DATAPATH="static/data/data.csv"
-MAPNAME="map.html"
-if not os.path.isfile(DATAPATH):
-  writetxt(DATAPATH,"name,contact,lat,lng")
+
 class webpage():
-  @app.route("/cod2.html")
-  def cod2():
-    return render_template("cod2.html")
   @app.route("/cod1.html")
   def cod1():#Cels10428
     return render_template("cod1.html")
+  @app.route("/cod2.html")
+  def cod2():#Au10428
+    return render_template("cod2.html")
+  @app.route("/cod3.html")
+  def cod3():#Eaf10428
+    return render_template("cod3.html")
+  @app.route("/null.html")
+  def no():#Eaf10428
+    return render_template("no.html")
   @app.route("/",methods=["GET","POST"])
   def index():
     if request.method=="POST":
      code=request.form["code"]
+     code=code.title()
      if "Cels10428"==code:
       return redirect("cod1.html") 
      if "Arg10428"==code:
       return redirect("cod2.html")
+     if "Eaf10428"==code:
+      return redirect("cod3.html")
+     else:
+      return redirect("null.html")
     return render_template("index.html")
       
 if __name__ == "__main__":
